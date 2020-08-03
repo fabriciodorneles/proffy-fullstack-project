ef1e18857ba5631dc820c4ce490f5f543bee779c
Fabricio Dorneles <fabriciodorneles@gmail.com>
2020-08-03 16:39:17 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './assets/styles/global.css';
+import Landing from './pages/Landing';
+
+function App() {
+  return (
+    <Landing />
+  );
+}
+
+export default App;

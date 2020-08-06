f1ccdd8a58a08eea677c455919ace2305868a81f
Fabricio Dorneles <fabriciodorneles@gmail.com>
2020-08-06 13:26:11 -0300

    integration browser-api ok

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -2,33 +2,51 @@ import React from 'react';
 import './styles.css';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+export interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection(){
+        api.post('connections', {
+            user_id: teacher.id,
+        });
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars3.githubusercontent.com/u/64965851?s=460&u=b79bd5d2415bae7a386c0828c770872e0c6fc9db&v=4" alt="Fabricio Dorneles"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Fabricio Dorneles</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Entusiasta das melhores tecnologias de química avançada.
-                <br/><br/>
-                Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 20,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,18 +1,43 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import PageHeader from '../../components/PageHeader';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+import api from '../../services/api';
 
 import './styles.css';
 
-import TeacherItem from '../../components/TeacherItem';
-import Input from '../../components/Input';
-import Select from '../../components/Select';
+
 
 function TeacherList() {
+    const [teachers, setTeachers] =useState([]);
+    const [subject, setSubject] = useState('');
+    const [week_day, setWeekDay] = useState('');
+    const [time, setTime] = useState('');
+
+    async function searchTeachers(e: FormEvent) {
+        e.preventDefault();
+        const response = await api.get('classes', {
+            params: {
+                subject,
+                week_day,
+                time,
+            }
+        });
+        console.log(response.data);
+        setTeachers(response.data);
+    }
+
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Esses são os proffys disponíveis." >
-                <form id="search-teachers">
-                <Select name="subject" label="Matéria" options={[
+                <form id="search-teachers" onSubmit={searchTeachers}>
+                <Select 
+                    name="subject" 
+                    label="Matéria" 
+                    value = {subject}
+                    onChange = {e => {setSubject(e.target.value)}}
+                    options={[
                        { value: 'Artes', label: 'Artes'},
                        { value: 'Biologia', label: 'Biologia'},
                        { value: 'Ciências', label: 'Ciências'},
@@ -22,7 +47,13 @@ function TeacherList() {
                        { value: 'Matemática', label: 'Matemática'},
                        { value: 'Filosofia', label: 'Filosofia'},
                     ]} />
-                 <Select name="week_day" label="Dia da Semana" options={[
+                    
+                 <Select 
+                    name="week_day" 
+                    label="Dia da Semana" 
+                    value = {week_day}
+                    onChange = {e => {setWeekDay(e.target.value)}}
+                    options={[
                        { value: '0', label: 'Domingo'},
                        { value: '1', label: 'Segunda-Feira'},
                        { value: '2', label: 'Terça-Feira'},
@@ -31,15 +62,24 @@ function TeacherList() {
                        { value: '5', label: 'Sexta-Feira'},
                        { value: '6', label: 'Sábado'},
                     ]} />
-                    <Input type ="time" name="time" label="Hora"/>
+
+                    <Input 
+                        type ="time" 
+                        name="time" 
+                        label="Hora"
+                        value = {time}
+                        onChange = {e => {setTime(e.target.value)}}
+                    />
+                    <button type="submit">
+                        Buscar
+                    </button>
                 </form>
             </PageHeader>
 
             <main>
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-
+                    {teachers.map((teacher: Teacher) => {
+                        return <TeacherItem key={teacher.id} teacher={teacher}/>;
+                    })}
             </main>
         </div>
     )
